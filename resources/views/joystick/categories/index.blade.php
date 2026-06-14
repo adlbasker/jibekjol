822db276659b99ba23a89d4080ab914edb826e47
Adl Basker <issa.adilet@gmail.com>
2026-06-14 15:34:17 +0500

    grouped list of categories

--- resources/views/joystick/categories/index.blade.php
+++ resources/views/joystick/categories/index.blade.php
@@ -28,7 +28,6 @@
         <tr class="table-active">
           <th style="width: 40px;"><input type="checkbox" onclick="toggleCheckbox(this)" class="form-check-input checkbox-ids"></th>
           <th style="width: 50px;">№</th>
-          <th>Язык</th>
           <th>Название</th>
           <th>URI</th>
           <th>Номер</th>
@@ -38,34 +37,40 @@
       </thead>
       <tbody>
         <?php $i = 1; ?>
-        <?php $traverse = function ($nodes, $parent = null, $prefix = null, $caret = null) use (&$traverse, $lang, &$i, $__env) { ?>
-          <?php foreach ($nodes as $node) : ?>
-            <tr <?php if ($parent != null): $classes = $node->ancestors->pluck('id')->flatten()->join(' '); ?> class="collapse {{ $classes }} show" <?php endif; ?>>
-              <td><input type="checkbox" name="categories_id[]" value="{{ $node->id }}" class="form-check-input checkbox-ids"></td>
-              <td>{{ $i++ }}</td>
-              <td>{{ $node->lang }}</td>
-              <td
-                <?php if ($node->descendants->count() > 0): $caret = '<i class="material-icons align-middle me-1">expand_more</i>'; ?>
-                  class="node-title cursor-pointer" data-bs-toggle="collapse" data-bs-target=".{{ $node->id }}" aria-expanded="true" aria-controls="{{ $node->id }}"
-                <?php endif; ?>>
-                {!! $caret !!} {{ PHP_EOL.$prefix.' '.$node->title }} <?php $caret = null; ?>
-              </td>
-              <td>{{ $node->slug }}</td>
-              <td>{{ $node->sort_id }}</td>
-              <td class="text-{{ __('statuses.data.'.$node->status.'.style') }}">{{ __('statuses.data.'.$node->status.'.title') }}</td>
-              <td class="text-end">
-                <a class="btn btn-link btn-sm" href="{{ route('categories.edit', [$lang, $node->id]) }}" title="Редактировать"><i class="material-icons">mode_edit</i></a>
-                <form method="POST" action="{{ route('categories.destroy', [$lang, $node->id]) }}" accept-charset="UTF-8" class="btn-delete">
-                  @csrf
-                  @method('DELETE')
-                  <button type="submit" class="btn btn-link btn-sm" onclick="return confirm('Удалить запись?')"><i class="material-icons">clear</i></button>
-                </form>
-              </td>
-            </tr>
-            <?php $traverse($node->children, $node, $prefix.'___'); ?>
-          <?php endforeach; ?>
-        <?php }; ?>
-        <?php $traverse($categories); ?>
+        <?php $grouped = $categories->groupBy('lang'); ?>
+        @foreach ($grouped as $langKey => $langCategories)
+          <tr class="table-active text-uppercase">
+            <th></th>
+            <th colspan="7">{{ $langKey }}</th>
+          </tr>
+          <?php $traverse = function ($nodes, $parent = null, $prefix = null, $caret = null) use (&$traverse, &$i, $__env) { ?>
+            <?php foreach ($nodes as $node) : ?>
+              <tr <?php if ($parent != null): $classes = $node->ancestors->pluck('id')->flatten()->join(' '); ?> class="collapse {{ $classes }} show" <?php endif; ?>>
+                <td><input type="checkbox" name="categories_id[]" value="{{ $node->id }}" class="form-check-input checkbox-ids"></td>
+                <td>{{ $i++ }}</td>
+                <td
+                  <?php if ($node->descendants->count() > 0): $caret = '<i class="material-icons align-middle me-1">expand_more</i>'; ?>
+                    class="node-title cursor-pointer" data-bs-toggle="collapse" data-bs-target=".{{ $node->id }}" aria-expanded="true" aria-controls="{{ $node->id }}"
+                  <?php endif; ?>>
+                  {!! $caret !!} {{ PHP_EOL.$prefix.' '.$node->title }} <?php $caret = null; ?>
+                </td>
+                <td>{{ $node->slug }}</td>
+                <td>{{ $node->sort_id }}</td>
+                <td class="text-{{ __('statuses.data.'.$node->status.'.style') }}">{{ __('statuses.data.'.$node->status.'.title') }}</td>
+                <td class="text-end">
+                  <a class="btn btn-link btn-sm" href="{{ route('categories.edit', [app()->getLocale(), $node->id]) }}" title="Редактировать"><i class="material-icons">mode_edit</i></a>
+                  <form method="POST" action="{{ route('categories.destroy', [app()->getLocale(), $node->id]) }}" accept-charset="UTF-8" class="btn-delete">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-link btn-sm" onclick="return confirm('Удалить запись?')"><i class="material-icons">clear</i></button>
+                  </form>
+                </td>
+              </tr>
+              <?php $traverse($node->children, $node, $prefix.'___'); ?>
+            <?php endforeach; ?>
+          <?php }; ?>
+          <?php $traverse($langCategories); ?>
+        @endforeach
       </tbody>
     </table>
   </div>
